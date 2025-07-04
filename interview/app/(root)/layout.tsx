0396b257f4a7e33601dcb538bf229b69e5e4ca97
yashrepogit <yas@cloudify.biz>
2025-07-04 22:08:20 +0530

    project finalized

--- interview/app/(root)/layout.tsx
+++ interview/app/(root)/layout.tsx
@@ -14,8 +14,8 @@ const Rootlayout = async ({children}:{children: ReactNode}) => {
                 <Image src="/logo.svg" alt="Logo" width={38} height={32} />
                 <h2 className='text-primary-100'>PrepInterview</h2>
             </Link>
-            {children}
         </nav>
+        {children}
     </div>
   )
 }
